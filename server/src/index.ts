33b2a748d5d144fa6ff0eb60808062283528babd
IvanDanyliuk <ivandaniliuk@gmail.com>
2025-03-10 14:53:49 +0200

    server setup

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+import authRoutes from "./routes/auth.route";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+app.use(cors());
+app.use(express.json());
+app.use("api/auth", authRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
