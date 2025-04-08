c57553cadd619a1c8798f5b101dfce689953e27a
IvanDanyliuk <ivandaniliuk@gmail.com>
2025-04-08 15:41:10 +0300

    fix the data table component

--- client/src/features/admin/pages/Properties/Properties.tsx
+++ client/src/features/admin/pages/Properties/Properties.tsx
@@ -73,8 +73,10 @@ const PropertiesPage = () => {
   }, []);
 
   useEffect(() => {
-    if(isSuccess) dispatch(setProperties(data.properties));
-  }, [isSuccess, dispatch])
+    if(data && data.properties) {
+      dispatch(setProperties(data.properties));
+    }
+  }, [isSuccess, dispatch, searchParams, data]);
 
   return (
     <AdminPageContainer 
