b242866d9f34439bd548596f7065d3c632096941
IvanDanyliuk <ivandaniliuk@gmail.com>
2025-04-14 16:41:45 +0300

    add article model, schema, and services

---
+++ server/src/services/article.service.ts
@@ -0,0 +1,73 @@
+import { deleteFromCloudinary, uploadToCloudinary } from "./cloudinary.service";
+import ArticleModel from "../models/article.schema";
+
+export type GetPropertiesParams = {
+  page: number,
+  itemsPerPage: number,
+}
+
+export const getArticles = async ({ page, itemsPerPage }: GetPropertiesParams) => {
+  const articles = await ArticleModel
+    .find({ page, itemsPerPage })
+    .sort({ "createdAt": -1 })
+    .skip((page - 1) * itemsPerPage)
+    .limit(itemsPerPage)
+    .exec();
+
+  const count = await ArticleModel.countDocuments();
+
+  return {
+    articles,
+    count,
+  };
+};
+
+interface NewArticleParams {
+  title: string;
+  content: string;
+  images?: any[];
+};
+
+export const createArticle = async (data: NewArticleParams) => {
+  const uploadedImages = data.images && data.images.length > 0 
+    ? await Promise.all(data.images.map((image: any) => uploadToCloudinary(image.buffer))) 
+    : [];
+
+  const newArticle = await ArticleModel.create({
+    ...data,
+    images: uploadedImages,
+  });
+
+  return newArticle;
+};
+
+interface UpdateArticleParams  extends NewArticleParams {
+  _id: string;
+}
+
+export const updateExistingArticle = async (articleToUpdate: UpdateArticleParams) => {
+  const existingArticle = await ArticleModel.findById(articleToUpdate._id);
+
+  const updatedImages = articleToUpdate.images && articleToUpdate.images.length > 0 
+    ? await Promise.all(articleToUpdate.images.map((image: any) => uploadToCloudinary(image.buffer))) 
+    : null;
+
+  const updatedArticle = await ArticleModel.findByIdAndUpdate(articleToUpdate._id, {
+    ...articleToUpdate,
+    images: updatedImages || existingArticle!.images,
+  });
+
+  return updatedArticle;
+};
+
+export const deleteProperty = async (id: string) => {
+  const deletedArticle = await ArticleModel.findByIdAndDelete(id);
+
+  if(deletedArticle) {
+    await deleteFromCloudinary(deletedArticle.images);
+  }
+
+  return {
+    message: "Article has been successfully deleted!",
+  };
+};
