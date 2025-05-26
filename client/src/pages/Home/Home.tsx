d2d211a09017c2b9ce0002ea33c250e611511975
IvanDanyliuk <ivandaniliuk@gmail.com>
2025-05-26 17:44:53 +0300

    fix the like/unlike property feature

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,71 +1,73 @@
+import { useCallback, useEffect } from 'react';
 import { Link } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import { Box, Container, Typography } from '@mui/material';
+import Arrow from '@mui/icons-material/East';
+import { StyleProps } from '../../components/types';
 import { SearchBox } from '../../features/properties/components/SearchBox/SearchBox';
 import { PropertyTypesList } from '../../features/properties/components/PropertyTypesList/PropertyTypesList';
 import { PropertyLocations } from '../../features/properties/components/PropertyLocations/PropertyLocations';
-import { StyleProps } from '../../components/types';
-import videoBg from '../../assets/video/hero-bg.mp4';
-import Arrow from '@mui/icons-material/East';
 import { useLazyGetPopularPropertiesQuery, useUpdatePropertyMutation } from '../../features/properties/state/propertyApi';
-import { styles } from './styles';
 import { PropertyList } from '../../features/properties/components/PropertyList/PropertyList';
-import { useAppSelector } from '../../hooks/useAppSelector';
-import { useCallback, useEffect } from 'react';
 import { useUpdateUserMutation } from '../../features/users/state/userApi';
 import { PropertyType } from '../../features/properties/state/types';
+import { setUser } from '../../features/users/state/userSlice';
+import { useAppDispatch } from '../../hooks/useAppDispatch';
+import { useAppSelector } from '../../hooks/useAppSelector';
+import videoBg from '../../assets/video/hero-bg.mp4';
+import { styles } from './styles';
 
 
 const HomePage = () => {
   const { t } = useTranslation();
+  const dispatch = useAppDispatch();
 
   const [ getProperties, { data, isSuccess: isGetPropertiesSuccess } ] = useLazyGetPopularPropertiesQuery();
   const [updateExistingProperty, { isSuccess: isUpdatePropertySuccess }] = useUpdatePropertyMutation();
   const [updateUser, { isSuccess: isUpdateUserSuccess }] = useUpdateUserMutation();
   
-  const { user } = useAppSelector(state => state.user)
+  const { user } = useAppSelector(state => state.user);
   
   const handleLikeProperty = useCallback(async (property: PropertyType) => {
     if(user) {
-      const isPropertyLiked = user.likedProperties.find(propertyId => propertyId === property._id);
+      const isPropertyLiked = user.likedProperties.includes(property._id);
+
+      let updatedProperty;
+      let updatedUser;
+
       if(isPropertyLiked) {
-        await updateExistingProperty({
+        updatedProperty = {
           ...property,
           location: JSON.stringify(property.location),
           overview: JSON.stringify(property.overview),
           nearbyAmenities: JSON.stringify(property.nearbyAmenities),
           likes: property.likes.filter((userId: string) => userId !== user._id)
-        });
+        };
         const updatedLikeProperties = user.likedProperties.filter(propertyId => propertyId !== property._id);
-        await updateUser({
+        updatedUser = {
           ...user,
           likedProperties: updatedLikeProperties
-        })
+        }
       } else {
-        console.log({
-          updatedProperty: {
-            ...property,
-            likes: [...property.likes, user._id]
-          },
-          updatedUser: {
-            ...user,
-            likedProperties: [...user.likedProperties, property._id]
-          }
-        })
-        await updateExistingProperty({
+        updatedProperty = {
           ...property,
           location: JSON.stringify(property.location),
           overview: JSON.stringify(property.overview),
           nearbyAmenities: JSON.stringify(property.nearbyAmenities),
           likes: [...property.likes, user._id]
-        });
-        await updateUser({
+        };
+        updatedUser = {
           ...user,
           likedProperties: [...user.likedProperties, property._id]
-        })
+        }
+      }
+      await updateExistingProperty(updatedProperty);
+      const { data, error } = await updateUser(updatedUser);
+      if(!error) {
+        dispatch(setUser(data.payload));
       }
     }
-  }, []);
+  }, [updateExistingProperty, updateUser, user]);
 
   useEffect(() => {
     getProperties({ limit: 8 });
