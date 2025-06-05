534db82398c9223c3c838bc9c9e9ef9998c1db43
IvanDanyliuk <ivandaniliuk@gmail.com>
2025-06-05 16:37:36 +0300

    add the liked properties list

--- client/src/features/users/pages/LikedProperties/LikedProperties.tsx
+++ client/src/features/users/pages/LikedProperties/LikedProperties.tsx
@@ -1,9 +1,19 @@
 import { Box } from "@mui/material"
+import { PropertyList } from "../../../properties/components/PropertyList/PropertyList";
+import { useAppSelector } from "../../../../hooks/useAppSelector";
+import { Loader } from "../../../../components/layout/Loader/Loader";
+import { styles } from "./styles";
 
 const LikedPropertiesPage = () => {
+  const { user } = useAppSelector(state => state.user);
+  console.log('LIKED PROPERTIES', user)
+  if(!user) {
+    return <Loader />;
+  }
+
   return (
-    <Box>
-      Liked Properties
+    <Box sx={styles.container}>
+      <PropertyList data={user.likedProperties} />
     </Box>
   );
 };
---
+++ client/src/features/users/pages/LikedProperties/styles.ts
@@ -0,0 +1,7 @@
+import { StyleProps } from '../../../../components/types';
+
+export const styles: StyleProps = {
+  container: {
+    padding: '1rem 0',
+  },
+};
