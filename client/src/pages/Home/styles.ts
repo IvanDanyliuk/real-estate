809e9ff12cc3875a2a76998066e2b6cb0259cd97
IvanDanyliuk <ivandaniliuk@gmail.com>
2025-06-09 14:02:47 +0300

    add responsive styles for the Hero section of the Home page

--- client/src/pages/Home/styles.ts
+++ client/src/pages/Home/styles.ts
@@ -8,6 +8,7 @@ export const styles: StyleProps = {
   },
   hero: {
     position: 'relative',
+    padding: '0 1rem',
     width: '100%',
     minHeight: '100%  ',
     overflow: 'hidden',
@@ -21,14 +22,16 @@ export const styles: StyleProps = {
       backgroundColor: 'rgba(0, 0, 0, 0.3 )',
       zIndex: -1,
     },
+    '@media(min-width:600px)': {
+      padding: 0,
+    },
   },
   introduction: {
-    position: 'absolute',
-    top: '10vh',
-    left: 0,
+    position: 'relative',
+    margin: '20rem 0 10rem 0',
     padding: '2rem',
     maxWidth: '35rem',
-    backgroundColor: 'primary.light',
+    backgroundColor: 'rgba(255,255,255,0.8)',
     borderRadius: '10px',
     '& h1': {
       marginTop: '1.5rem',
@@ -69,7 +72,54 @@ export const styles: StyleProps = {
         fontWeight: 600,
         color: 'primary.main',
       }
-    }
+    },
+    '@media(min-width:600px)': {
+      position: 'absolute',
+      top: '10vh',
+      left: 0,
+      margin: 0,
+      backgroundColor: 'primary.light',
+      '& h1': {
+        marginTop: '1.5rem',
+        fontSize: '2rem',
+        fontWeight: 500,
+        fontStyle: 'italic',
+        lineHeight: '1.5rem',
+        '& span': {
+          display: 'block',
+          fontSize: '2rem',
+          fontWeight: 600,
+          fontStyle: 'normal',
+          color: 'primary.main'
+        }
+      },
+      '& h3': {
+        display: 'flex',
+        alignItems: 'center',
+        gap: 1,
+        fontSize: '1.1rem',
+        fontWeight: 500,
+        '&:before': {
+          content: '""',
+          width: '3rem',
+          height: '2px',
+          backgroundColor: 'primary.main'
+        }
+      },
+      '& p': {
+        margin: '1.2rem 0',
+      },
+      '& a': {
+        fontSize: '1.3rem',
+        color: 'secondary.dark',
+        '& span': {
+          marginLeft: '0.3rem',
+          fontSize: '1.3rem',
+          fontWeight: 600,
+          color: 'primary.main',
+        }
+      },
+    },
   },
   sectionHeading: {
     padding: '1rem',
