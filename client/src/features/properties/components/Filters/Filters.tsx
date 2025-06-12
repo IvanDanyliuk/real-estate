e1f19cc9aaf2663c53297f530b6f58a85e93e3dc
IvanDanyliuk <ivandaniliuk@gmail.com>
2025-06-12 13:34:21 +0300

    add the price range and area filters

--- client/src/features/properties/components/Filters/Filters.tsx
+++ client/src/features/properties/components/Filters/Filters.tsx
@@ -1,5 +1,5 @@
 import { useSearchParams } from 'react-router-dom';
-import { Box, Checkbox, Divider, FormControlLabel, FormGroup, MenuItem, Select, Typography } from '@mui/material';
+import { Box, Button, Checkbox, Divider, FormControlLabel, FormGroup, MenuItem, Select, Slider, Typography } from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { AD_TYPES, MARKET_TYPES, PROPERTY_TYPES } from '../../../../constants/main';
@@ -19,9 +19,13 @@ export const Filters: React.FC = () => {
 
   const propertyTypeFromParams = searchParams.get('propertyType')?.split(',') || [];
   const marketTypeFromParams = searchParams.get('marketType')?.split(',') || [];
+  const priceFromParams = searchParams.get('price')?.split(',').map(Number) || [26000, 185000];
+  const areaFromParams = searchParams.get('area')?.split(',').map(Number) || [26, 470];
 
-  const defaultValues: Record<string, boolean | string[]> = {};
+  const defaultValues: Record<string, boolean | string[] | number[]> = {};
 
+  defaultValues['price'] = priceFromParams;
+  defaultValues['area'] = areaFromParams;
   defaultValues['location'] = [REGIONS_DEFAULT.value];
 
   PROPERTY_TYPES.forEach(({ value }) => {
@@ -32,10 +36,14 @@ export const Filters: React.FC = () => {
     defaultValues[value] = marketTypeFromParams.includes(value);
   });
 
-  const { control } = useForm({ defaultValues });
+  const { control, reset } = useForm({ defaultValues });
 
-  const handleInputFiltersChange = (key: string, value: string | number) => {
-    searchParams.set(key, value.toString());
+  const handleInputFiltersChange = (key: string, value: string | number | (string | number)[]) => {
+    if (Array.isArray(value)) {
+      searchParams.set(key, value.join(','));
+    } else {
+      searchParams.set(key, value.toString());
+    }
     setSearchParams(searchParams);
   };
 
@@ -56,6 +64,12 @@ export const Filters: React.FC = () => {
     }
 
     setSearchParams(searchParams);
+  };
+
+  const handleClearFilters = () => {
+    const newSearchParams = new URLSearchParams();
+    setSearchParams(newSearchParams);
+    reset(defaultValues);
   };
 
   return (
@@ -179,7 +193,83 @@ export const Filters: React.FC = () => {
             />
           ))}
         </FormGroup>
-
+        <Divider />
+        <FormGroup>
+          <Typography 
+            variant='h6' 
+            sx={styles.sectionName}
+          >
+            {t('pages.properties.filters.sections.price.title')}
+          </Typography>
+          <Controller 
+            name='price'
+            control={control}
+            render={({ field }) => (
+              <>
+                <Slider
+                  {...field}
+                  value={field.value as any}
+                  onChange={(_, value) => {
+                    field.onChange(value);
+                    handleInputFiltersChange('price', value as number[]);
+                  }}
+                  valueLabelDisplay='auto'
+                  min={0}
+                  max={200000}
+                  step={1000}
+                />
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                  <Typography variant='body2'>
+                    {typeof field.value === 'object' ? field.value[0] : ''}
+                  </Typography>
+                  <Typography variant='body2'>
+                    {typeof field.value === 'object' ? field.value[1] : ''}
+                  </Typography>
+                </Box>
+              </>
+            )}
+          />
+        </FormGroup>
+        <Divider />
+        <FormGroup>
+          <Typography 
+            variant='h6' 
+            sx={styles.sectionName}
+          >
+            {t('pages.properties.filters.sections.area.title')}
+          </Typography>
+          <Controller 
+            name='area'
+            control={control}
+            render={({ field }) => (
+              <>
+                <Slider
+                  {...field}
+                  value={field.value as any}
+                  onChange={(_, value) => {
+                    field.onChange(value);
+                    handleInputFiltersChange('area', value as number[]);
+                  }}
+                  valueLabelDisplay='auto'
+                  min={0}
+                  max={1000}
+                  step={1}
+                />
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                  <Typography variant='body2'>
+                    {typeof field.value === 'object' ? field.value[0] : ''}
+                  </Typography>
+                  <Typography variant='body2'>
+                    {typeof field.value === 'object' ? field.value[1] : ''}
+                  </Typography>
+                </Box>
+              </>
+            )}
+          />
+        </FormGroup>
+        <Button onClick={handleClearFilters}>
+          Clear filters
+        </Button>
       </Box>
     </Box>
   );
