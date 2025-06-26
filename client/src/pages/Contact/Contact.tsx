a13c72477c02c77427c77bf3c8ade281cddd9f84
IvanDanyliuk <ivandaniliuk@gmail.com>
2025-06-26 20:32:45 +0300

    add the Contacts page

--- client/src/pages/Contact/Contact.tsx
+++ client/src/pages/Contact/Contact.tsx
@@ -1,8 +1,23 @@
+import { useTranslation } from 'react-i18next';
+import { Container } from '../../components/layout/Container/Container';
+import { Typography } from '@mui/material';
+import { SocialMediaLinks } from '../../components/layout/SocialMediaLinks/SocialMediaLinks';
+import { styles } from './styles';
+
+
 const ContactPage = () => {
+  const { t } = useTranslation();
+
   return (
-    <div>
-      Contact page
-    </div>
+    <Container contentStyles={styles.container}>
+      <Typography variant='h1'>
+        {t('pages.contacts.mainHeading')}
+      </Typography>
+      <Typography>
+        {t('pages.contacts.subHeading')}
+      </Typography>
+      <SocialMediaLinks />
+    </Container>
   );
 };
 
--- client/src/pages/Contact/styles.ts
+++ client/src/pages/Contact/styles.ts
@@ -1,5 +1,25 @@
 import { StyleProps } from '../../components/types';
 
-export const styles: StyleProps = {
 
+export const styles: StyleProps = {
+  container: {
+    width: '100%',
+    height: '70vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 3,
+    '& h1': {
+      fontSize: '3rem',
+      fontWeight: 500,
+      textAlign: 'center',
+      color: 'primary.main',
+    },
+    '& p': {
+      fontSize: '1.2rem',
+      textAlign: 'center',
+      color: 'secondary.main',
+    }
+  }
 };
